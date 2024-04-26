2407820170efe30726eab33a0516d3d5eba68522
kubab1el4 <kubab1el4nski@gmail.com>
2024-04-26 21:30:48 +0200

    fix: make better filter and sort controller functionality

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,52 +8,66 @@ use Meilisearch\Endpoints\Indexes;
 
 class SearchController extends Controller {
     public function searchSets(Request $request) {
-        $sets = LEGOSet::search($request->search)->get();
+        $sets = LEGOSet::search($request->search)->get()->toQuery();
 
         if ($request->has('filter')) {
             $filterArray = explode(',', $request->get('filter'));
             $count = 1;
             foreach($filterArray as $filter) {
-                $matches = [];
-                preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
-                $operator = $matches[0];
-                $filterStrings = explode($operator, $filter);
-                if ($count === 1) {
-                    $sets = $sets->where($filterStrings[0], $operator, $filterStrings[1]);
-                } else {
-                    $sets = $sets->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+                if ($filter !== "") {
+                    $matches = [];
+                    preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
+                    $operator = $matches[0];
+                    $filterStrings = explode($operator, $filter);
+                    if ($count === 1) {
+                        $sets = $sets->where($filterStrings[0], $operator, $filterStrings[1]);
+                    } else {
+                        $sets = $sets->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+                    }
                 }
+                $count++;
             }
         }
-        if ($request->has('sort') && !empty($sets->toArray())) {
-            $sortArray = explode(':', $request->get('sort'));
-            $sets = $sets->toQuery()->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
+        if (!empty($sets->get()->toArray())) {
+            if ($request->has('sort') && $request->get('sort') !== "") {
+                $sortArray = explode(':', $request->get('sort'));
+                $sets = $sets->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
+            } else {
+                $sets = $sets->paginate(config('app.default_pagination'));
+            }
         }
 
         return $sets;
     }
 
     public function searchOffers(Request $request) {
-        $offers = LEGOSet::search($request->search)->get();
+        $offers = LEGOSet::search($request->search)->get()->toQuery();
 
         if ($request->has('filter')) {
             $filterArray = explode(',', $request->get('filter'));
             $count = 1;
             foreach($filterArray as $filter) {
-                $matches = [];
-                preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
-                $operator = $matches[0];
-                $filterStrings = explode($operator, $filter);
-                if ($count === 1) {
-                    $offers = $offers->where($filterStrings[0], $operator, $filterStrings[1]);
-                } else {
-                    $offers = $offers->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+                if ($filter !== "") {
+                    $matches = [];
+                    preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
+                    $operator = $matches[0];
+                    $filterStrings = explode($operator, $filter);
+                    if ($count === 1) {
+                        $offers = $offers->where($filterStrings[0], $operator, $filterStrings[1]);
+                    } else {
+                        $offers = $offers->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+                    }
                 }
+                $count++;
             }
         }
-        if ($request->has('sort') && !empty($offers->toArray())) {
-            $sortArray = explode(':', $request->get('sort'));
-            $offers = $offers->toQuery()->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
+        if (!empty($offers->get()->toArray())) {
+            if ($request->has('sort') && $request->get('sort') !== "") {
+                $sortArray = explode(':', $request->get('sort'));
+                $offers = $offers->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
+            } else {
+                $offers = $offers->paginate(config('app.default_pagination'));
+            }
         }
 
         return $offers;
