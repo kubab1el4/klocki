38f619f8051ccb7fb9b7d9a87fb25816a685f358
kubab1el4 <kubab1el4nski@gmail.com>
2024-04-27 17:07:42 +0200

    fix: accomodate the possibility of search returning 0 results

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,32 +8,36 @@ use Meilisearch\Endpoints\Indexes;
 
 class SearchController extends Controller {
     public function searchSets(Request $request) {
-        $sets = LEGOSet::search($request->search)->take(200)->get()->toQuery();
+        $sets = LEGOSet::search($request->search)->take(200)->get();
 
-        if ($request->has('filter')) {
-            $filterArray = explode(',', $request->get('filter'));
-            $count = 1;
-            foreach($filterArray as $filter) {
-                if ($filter !== "") {
-                    $matches = [];
-                    preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
-                    $operator = $matches[0];
-                    $filterStrings = explode($operator, $filter);
-                    if ($count === 1) {
-                        $sets = $sets->where($filterStrings[0], $operator, $filterStrings[1]);
-                    } else {
-                        $sets = $sets->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+        if($sets->count() !== 0) {
+            $sets = $sets->toQuery();
+
+            if ($request->has('filter')) {
+                $filterArray = explode(',', $request->get('filter'));
+                $count = 1;
+                foreach($filterArray as $filter) {
+                    if ($filter !== "") {
+                        $matches = [];
+                        preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
+                        $operator = $matches[0];
+                        $filterStrings = explode($operator, $filter);
+                        if ($count === 1) {
+                            $sets = $sets->where($filterStrings[0], $operator, $filterStrings[1]);
+                        } else {
+                            $sets = $sets->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+                        }
                     }
+                    $count++;
                 }
-                $count++;
             }
-        }
-        if (!empty($sets->get()->toArray())) {
-            if ($request->has('sort') && $request->get('sort') !== "") {
-                $sortArray = explode(':', $request->get('sort'));
-                $sets = $sets->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
-            } else {
-                $sets = $sets->paginate(config('app.default_pagination'));
+            if (!empty($sets->get()->toArray())) {
+                if ($request->has('sort') && $request->get('sort') !== "") {
+                    $sortArray = explode(':', $request->get('sort'));
+                    $sets = $sets->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
+                } else {
+                    $sets = $sets->paginate(config('app.default_pagination'));
+                }
             }
         }
 
@@ -41,32 +45,36 @@ class SearchController extends Controller {
     }
 
     public function searchOffers(Request $request) {
-        $offers = LEGOSet::search($request->search)->take(200)->get()->toQuery();
+        $offers = LEGOSet::search($request->search)->take(200)->get();
 
-        if ($request->has('filter')) {
-            $filterArray = explode(',', $request->get('filter'));
-            $count = 1;
-            foreach($filterArray as $filter) {
-                if ($filter !== "") {
-                    $matches = [];
-                    preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
-                    $operator = $matches[0];
-                    $filterStrings = explode($operator, $filter);
-                    if ($count === 1) {
-                        $offers = $offers->where($filterStrings[0], $operator, $filterStrings[1]);
-                    } else {
-                        $offers = $offers->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+        if($offers->count() !== 0) {
+            $sets = $offers->toQuery();
+
+            if ($request->has('filter')) {
+                $filterArray = explode(',', $request->get('filter'));
+                $count = 1;
+                foreach($filterArray as $filter) {
+                    if ($filter !== "") {
+                        $matches = [];
+                        preg_match('/[^a-zA-Z\d\s_\-:]{1,2}/', $filter, $matches);
+                        $operator = $matches[0];
+                        $filterStrings = explode($operator, $filter);
+                        if ($count === 1) {
+                            $offers = $offers->where($filterStrings[0], $operator, $filterStrings[1]);
+                        } else {
+                            $offers = $offers->orWhere($filterStrings[0], $operator, $filterStrings[1]);
+                        }
                     }
+                    $count++;
                 }
-                $count++;
             }
-        }
-        if (!empty($offers->get()->toArray())) {
-            if ($request->has('sort') && $request->get('sort') !== "") {
-                $sortArray = explode(':', $request->get('sort'));
-                $offers = $offers->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
-            } else {
-                $offers = $offers->paginate(config('app.default_pagination'));
+            if (!empty($offers->get()->toArray())) {
+                if ($request->has('sort') && $request->get('sort') !== "") {
+                    $sortArray = explode(':', $request->get('sort'));
+                    $offers = $offers->orderBy($sortArray[0], $sortArray[1])->paginate(config('app.default_pagination'));
+                } else {
+                    $offers = $offers->paginate(config('app.default_pagination'));
+                }
             }
         }
 
