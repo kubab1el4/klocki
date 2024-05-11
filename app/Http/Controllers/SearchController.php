9ac49e8f71d2524915bcb8ee1d2e8ce86eb6cfe3
kubab1el4 <kubab1el4nski@gmail.com>
2024-05-11 14:17:14 +0200

    fix: add back pagination to search results fix for scenario with 0 results

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,12 +10,22 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller {
     public function searchSets(Request $request) {
-        $search = LEGOSet::search($request->search)->take(100)->get()->toQuery()->pluck('id');
-        return SetResource::collection(LEGOSet::sort()->filter()->findMany($search->all()));
+        $search = LEGOSet::search($request->search)->take(200)->get();
+        if (count($search) !== 0) {
+            $search = $search->toQuery()->pluck('id');
+        } else {
+            return SetResource::collection($search);
+        }
+        return SetResource::collection(LEGOSet::sort()->filter()->findMany($search->all())->toQuery()->paginate(config('app.default_pagination')));
     }
 
     public function searchOffers(Request $request) {
-        $search = Offer::search($request->search)->take(100)->get()->toQuery()->pluck('id');
-        return OfferResource::collection(LEGOSet::sort()->filter()->findMany($search->all()));
+        $search = Offer::search($request->search)->take(200)->get();
+        if (count($search) !== 0) {
+            $search = $search->toQuery()->pluck('id');
+        } else {
+            return OfferResource::collection($search);
+        }
+        return OfferResource::collection(LEGOSet::sort()->filter()->findMany($search->all())->toQuery()->paginate(config('app.default_pagination')));
     }
 }
