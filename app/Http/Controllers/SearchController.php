1d682731e4297786c59ffd95c34bd8988d4dbe80
kubab1el4 <kubab1el4nski@gmail.com>
2024-05-18 18:43:59 +0200

    feat: change metadata added to json responses

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,19 +17,48 @@ class SearchController extends Controller {
             return SetResource::collection($search);
         }
         $result = LEGOSet::filter()->sort()->findMany($search->all());
-        $maxYear = $result->max('year');
-        $minYear = $result->min('year');
-        $maxElements = $result->max('num_parts');
-        $minElements = $result->min('num_parts');
-        $themeIds = array_values($result->pluck('theme_id')->unique()->toArray());
+
+        $yearsPluck = $result->pluck('year');
+        $yearsRet = [];
+        foreach ($yearsPluck as $year) {
+            $yearsRet[$year] = ['number_of_appearances' => (!empty($yearsRet[$year]['number_of_appearances']) ? $yearsRet[$year]['number_of_appearances'] + 1 : 1)];
+        }
+        $yearsRet = json_encode($yearsRet);
+
+        $elementsPluck = $result->pluck('num_parts');
+        $elementsCategories = ['0', '1-99','100-249', '250-499', '500-999', '1000+'];
+        $elementsRet = ['0' => [], '1-99' => [],'100-249' => [], '250-499' => [], '500-999' => [], '1000+' => []];
+        foreach ($elementsPluck as $elements) {
+            $elementsCategory = array_filter($elementsCategories, function($category) use ($elements) {
+                $range = explode('-', $category);
+                $rangeStart = $range[0];
+                $rangeEnd = $range[1] ?? 0;
+                if (!str_contains($rangeStart, '+')) {
+                    return $elements >= $rangeStart && $elements <= $rangeEnd;
+                } else {
+                    return $elements >= (int)preg_replace("/[^0-9]/", "", $rangeStart);
+                }
+            });
+            if (!empty($elementsCategory)) {
+                $elementsCategory = $elementsCategory[array_key_first($elementsCategory)];
+            }
+            $elementsRet[$elementsCategory] = ['number_of_appearances' => (!empty($elementsRet[$elementsCategory]['number_of_appearances']) ? $elementsRet[$elementsCategory]['number_of_appearances'] + 1 : 1)];
+        }
+        $elementsRet = json_encode($elementsRet);
+
+        $themesPluck = $result->pluck('theme_id');
+        $themesRet = [];
+        foreach ($themesPluck as $theme) {
+            $themesRet[$theme] = ['number_of_appearances' => (!empty($themesRet[$theme]['number_of_appearances']) ? $themesRet[$theme]['number_of_appearances'] + 1 : 1)];
+        }
+        $themesRet = json_encode($themesRet);
+
         $result = $result->paginate(config('app.default_pagination'));
 
         return SetResource::collection($result)->additional([
-            'max_year' => $maxYear,
-            'min_year' => $minYear,
-            'max_elements' => $maxElements,
-            'min_elements' => $minElements,
-            'theme_ids' => $themeIds
+            'years' => $yearsRet,
+            'elements' => $elementsRet,
+            'theme_ids' => $themesRet
         ]);
     }
 
--- app/Http/Controllers/SetController.php
+++ app/Http/Controllers/SetController.php
@@ -13,17 +13,48 @@ class SetController extends Controller
      */
     public function index() {
         $result = LEGOSet::sort()->filter()->sortFields('year')->get();
-        $maxYear = $result->max('year');
-        $minYear = $result->min('year');
-        $maxElements = $result->max('num_parts');
-        $minElements = $result->min('num_parts');
+
+        $yearsPluck = $result->pluck('year');
+        $yearsRet = [];
+        foreach ($yearsPluck as $year) {
+            $yearsRet[$year] = ['number_of_appearances' => (!empty($yearsRet[$year]['number_of_appearances']) ? $yearsRet[$year]['number_of_appearances'] + 1 : 1)];
+        }
+        $yearsRet = json_encode($yearsRet);
+
+        $elementsPluck = $result->pluck('num_parts');
+        $elementsCategories = ['0', '1-99','100-249', '250-499', '500-999', '1000+'];
+        $elementsRet = ['0' => [], '1-99' => [],'100-249' => [], '250-499' => [], '500-999' => [], '1000+' => []];
+        foreach ($elementsPluck as $elements) {
+            $elementsCategory = array_filter($elementsCategories, function($category) use ($elements) {
+                $range = explode('-', $category);
+                $rangeStart = $range[0];
+                $rangeEnd = $range[1] ?? 0;
+                if (!str_contains($rangeStart, '+')) {
+                    return $elements >= $rangeStart && $elements <= $rangeEnd;
+                } else {
+                    return $elements >= (int)preg_replace("/[^0-9]/", "", $rangeStart);
+                }
+            });
+            if (!empty($elementsCategory)) {
+                $elementsCategory = $elementsCategory[array_key_first($elementsCategory)];
+            }
+            $elementsRet[$elementsCategory] = ['number_of_appearances' => (!empty($elementsRet[$elementsCategory]['number_of_appearances']) ? $elementsRet[$elementsCategory]['number_of_appearances'] + 1 : 1)];
+        }
+        $elementsRet = json_encode($elementsRet);
+
+        $themesPluck = $result->pluck('theme_id');
+        $themesRet = [];
+        foreach ($themesPluck as $theme) {
+            $themesRet[$theme] = ['number_of_appearances' => (!empty($themesRet[$theme]['number_of_appearances']) ? $themesRet[$theme]['number_of_appearances'] + 1 : 1)];
+        }
+        $themesRet = json_encode($themesRet);
+
         $result = $result->paginate(config('app.default_pagination'));
 
         return SetResource::collection($result)->additional([
-            'max_year' => $maxYear,
-            'min_year' => $minYear,
-            'max_elements' => $maxElements,
-            'min_elements' => $minElements,
+            'years' => $yearsRet,
+            'elements' => $elementsRet,
+            'theme_ids' => $themesRet
         ]);
     }
 
