321c955de8026c6d5d569ae318b4289adb863274
kubab1el4 <kubab1el4nski@gmail.com>
2024-05-28 19:32:29 +0200

    fix: allow getting user data

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\OfferController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SetController;
 use App\Http\Controllers\ThemeController;
+use App\Http\Controllers\UserController;
 use App\Models\LEGOSet;
 use App\Models\Offer;
 use Illuminate\Http\Request;
@@ -15,23 +16,25 @@ use Meilisearch\Client;
 use Meilisearch\Endpoints\Indexes;
 use Meilisearch\Meilisearch;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 
-Route::get('hero', [ImageController::class, 'getThumbnail']);
+    Route::get('hero', [ImageController::class, 'getThumbnail']);
 
-Route::get('thumbnail/{set_num}', [ImageController::class, 'getThumbnail']);
+    Route::get('thumbnail/{set_num}', [ImageController::class, 'getThumbnail']);
 
-Route::get('set/{id}', [SetController::class, 'show']);
-Route::get('sets', [SetController::class, 'index']);
+    Route::get('set/{id}', [SetController::class, 'show']);
+    Route::get('sets', [SetController::class, 'index']);
 
-Route::get('theme/{id}', [ThemeController::class, 'show']);
-Route::get('theme/{id}/parent', [ThemeController::class, 'getParent']);
-Route::get('theme/{id}/children', [ThemeController::class, 'getChildren']);
-Route::get('themes', [ThemeController::class, 'index']);
+    Route::get('theme/{id}', [ThemeController::class, 'show']);
+    Route::get('theme/{id}/parent', [ThemeController::class, 'getParent']);
+    Route::get('theme/{id}/children', [ThemeController::class, 'getChildren']);
+    Route::get('themes', [ThemeController::class, 'index']);
 
-Route::get('offers', [OfferController::class, 'index']);
+    Route::get('offers', [OfferController::class, 'index']);
 
-Route::get('/search/sets', [SearchController::class, 'searchSets']);
-Route::get('/search/offers', [SearchController::class, 'searchOffers']);
+    Route::get('/search/sets', [SearchController::class, 'searchSets']);
+    Route::get('/search/offers', [SearchController::class, 'searchOffers']);
+    });
